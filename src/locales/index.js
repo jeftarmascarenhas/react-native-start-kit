b01fb623ad81caf139dd2525ae53051b7bd1dcb0
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2019-05-10 18:27:04 -0300

    add i18n js

--- src/locales/index.js
+++ src/locales/index.js
@@ -4,32 +4,28 @@ import en from './en-US'
 import pt from './pt-BR'
 
 const normalizeTranslate = {
-  'en_US': 'en_US',
-  'pt_BR': 'pt_BR',
-  'en': 'en_US',
-  'pt_US': 'pt_BR',
+  en_US: 'en_US',
+  pt_BR: 'pt_BR',
+  en: 'en_US',
+  pt_US: 'pt_BR',
 }
 
-const getLanguageByDevice = () => {
-  return Platform.OS === 'ios'
-    ? NativeModules.SettingsManager.settings.AppleLocale
-    : NativeModules.I18nManager.localeIdentifier
-}
+const getLanguageByDevice = () => (Platform.OS === 'ios'
+  ? NativeModules.SettingsManager.settings.AppleLocale
+  : NativeModules.I18nManager.localeIdentifier)
 
 I18n.fallback = true
 
 I18n.translations = {
-  'en_US': en,
-  'pt_BR': pt,
+  en_US: en,
+  pt_BR: pt,
 }
 
 const setLanguageToI18n = () => {
   const language = getLanguageByDevice()
   const translateNormalize = normalizeTranslate[language]
   const iHaveThisLanguage = I18n.translations.hasOwnProperty(translateNormalize)
-  iHaveThisLanguage
-    ? I18n.locale = translateNormalize
-    : I18n.defaultLocale = 'en_US'
+  iHaveThisLanguage ? (I18n.locale = translateNormalize) : (I18n.defaultLocale = 'en_US')
 }
 
 setLanguageToI18n()
